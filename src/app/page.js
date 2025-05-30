3f305f61b507549e96dc498d6e01744b0fbbd9e7
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-05-30 14:40:54 +0200

    hero

--- src/app/page.js
+++ src/app/page.js
@@ -85,11 +85,11 @@ export default async function Page() {
   <div className="absolute bottom-0 left-0 w-full h-[500px] bg-gradient-to-t from-black/80 to-black/0 z-0" />
 
   {/* Symbol (plusek) – může přesahovat, ale nebude se deformovat */}
-  <div className="absolute bottom-12 left-4 md:left-12 z-10 overflow-visible">
+  <div className="absolute bottom-20 md:bottom-12 left-4 md:left-12 z-10 overflow-visible">
     <img
       src="/images/symbol-outline.svg"
       alt=""
-      className="w-[120vw] max-w-none md:w-[736px]" // přesahuje viewport
+      className="w-[140vw] max-w-none md:w-[736px]" // přesahuje viewport
     />
   </div>
 
