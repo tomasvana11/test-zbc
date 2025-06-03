bf3af58777aaf8e437684ff55aafe62af9c5eb8f
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-03 21:21:16 +0200

    mobile

--- src/app/components/PageHeader.js
+++ src/app/components/PageHeader.js
@@ -1,7 +1,7 @@
 export default function PageHeader({ title, description }) {
   return (
     <section
-      className="relative w-full overflow-hidden bg-[left_center] bg-no-repeat pt-[100px]"
+      className="relative w-full overflow-hidden bg-[left_center] bg-no-repeat pt-[76px] md:pt-[100px]"
       style={{
         backgroundImage: "url('/images/heroimage.png')",
         backgroundSize: '120% auto',
@@ -16,16 +16,16 @@ export default function PageHeader({ title, description }) {
         }}
       />
       {/* Symbol (plusek) */}
-      <div className="absolute -top-[109px] left-12 z-10 overflow-visible">
+      <div className="absolute -top-[14px] md:-top-[109px] left-8 md:left-12 z-10 overflow-visible">
         <img
           src="/images/symbol-outline.svg"
           alt=""
-          className="w-[135vw] max-w-none md:w-[736px]"
+          className="max-w-none w-[400px] md:w-[736px]"
         />
       </div>
       {/* Text */}
-      <div className="relative z-20 flex flex-col gap-[32px] w-full max-w-[1392px] mx-auto py-12 items-center mt-[64px] mb-[144px]">
-        <h1 className="text-[56px] text-silkBeige text-center">{title}</h1>
+      <div className="relative z-20 flex flex-col gpa-[24px] md:gap-[32px] w-full max-w-[1392px] mx-auto py-8 md:py-12 items-center mt-[56px] md:mt-[64px] mb-[100px] md:mb-[144px]">
+        <h1 className="text-[48px] md:text-[56px] text-silkBeige text-center">{title}</h1>
         {description && (
           <p className="text-silkBeige text-center max-w-[700px]">{description}</p>
         )}
