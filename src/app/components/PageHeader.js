deae2f2157e43f67c7bcf06b7376188164c1d40d
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-03 21:32:00 +0200

    mobile fix

--- src/app/components/PageHeader.js
+++ src/app/components/PageHeader.js
@@ -1,12 +1,7 @@
 export default function PageHeader({ title, description }) {
   return (
     <section
-      className="relative w-full overflow-hidden bg-[left_center] bg-no-repeat pt-[76px] md:pt-[100px]"
-      style={{
-        backgroundImage: "url('/images/heroimage.png')",
-        backgroundSize: '120% auto',
-        backgroundPosition: 'right center',
-      }}
+      className="relative w-full overflow-hidden bg-[url('/images/heroimage.png')] bg-no-repeat bg-cover md:bg-[length:120%_auto] bg-[right_center] pt-[76px] md:pt-[100px]"
     >
       {/* Gradient */}
       <div
@@ -15,16 +10,18 @@ export default function PageHeader({ title, description }) {
           background: 'linear-gradient(to bottom, rgba(52, 61, 60, 0.8), rgba(151, 167, 165, 0.8))',
         }}
       />
+
       {/* Symbol (plusek) */}
-      <div className="absolute -top-[14px] md:-top-[109px] left-8 md:left-12 z-10 overflow-visible">
+      <div className="absolute -top-[14px] md:-top-[109px] left-4 md:left-12 z-10 overflow-visible">
         <img
           src="/images/symbol-outline.svg"
           alt=""
           className="max-w-none w-[450px] md:w-[736px]"
         />
       </div>
+
       {/* Text */}
-      <div className="relative z-20 flex flex-col gpa-[24px] md:gap-[32px] w-full max-w-[1392px] mx-auto py-8 md:py-12 items-center mt-[56px] md:mt-[64px] mb-[100px] md:mb-[144px]">
+      <div className="relative z-20 flex flex-col gap-[24px] md:gap-[32px] w-full max-w-[1392px] mx-auto py-8 md:py-12 items-center mt-[56px] md:mt-[64px] mb-[100px] md:mb-[144px]">
         <h1 className="text-[48px] md:text-[56px] text-silkBeige text-center">{title}</h1>
         {description && (
           <p className="text-silkBeige text-center max-w-[700px]">{description}</p>
