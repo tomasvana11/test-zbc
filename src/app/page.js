76494ff142a270b2954a7dd1652dfd0c43a39144
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-03 13:54:22 +0200

    clean code

--- src/app/page.js
+++ src/app/page.js
@@ -478,50 +478,6 @@ export default async function Page() {
 
 </section>
 
-
-<footer className="bg-raisinBlack w-full px-4">
-  <div className="flex flex-col md:flex-row w-full items-center max-w-[1392px] mx-auto justify-between py-12 md:py-16">
-    <div className="flex flex-row md:w-1/2 w-full justify-between">
-      <div className="md:w-1/3 items-start"><img src="/images/zbc-logo-vertical.svg" alt="Symbol Gold" className="w-[125px]" /></div>
-      <div className="md:w-2/3 items-start pr-8"><p className="text-silkBeige pl-8">Za bohatší Česko s.r.o<br />Jankovcova 1569/2c 170 00, Praha 7<br />+420 603 818 647 <br />info@zabohatsicesko.cz</p></div>
-    </div>
-    <div className="md:w-1/2 w-full items-start md:items-end mt-4 md:mt-0">
-      <h3 className="footer_h text-goldenBrown text-3xl text-left md:text-right">Sledujte nás</h3>
-      <div className="flex flex-row gap-2 items-start md:justify-end mt-4">
-        <div className="bg-socialDiv w-[36px] h-[36px] rounded">
-          <a href="https://www.facebook.com/Zabohatsicesko"><img src="/images/icon_fb.svg" alt="Facebook" className="w-[36px] h-[36px]" /></a>
-        </div>
-        <div className="bg-socialDiv w-[36px] h-[36px] rounded">
-          <a href="https://www.instagram.com/zabohatsicesko"><img src="/images/icon_ig.svg" alt="Instagram" className="w-[36px] h-[36px]" /></a>
-        </div>
-        <div className="bg-socialDiv w-[36px] h-[36px] rounded">
-          <a href="https://www.youtube.com/@zabohatsicesko"><img src="/images/icon_ytb.svg" alt="YouTube" className="w-[36px] h-[36px]" /></a>
-        </div>
-        <div className="bg-socialDiv w-[36px] h-[36px] rounded">
-          <a href="https://www.linkedin.com/company/za-bohatší-česko-s-r-o/about/"><img src="/images/icon_ln.svg" alt="LinkedIn" className="w-[36px] h-[36px]" /></a>
-        </div>
-        <div className="bg-socialDiv w-[36px] h-[36px] rounded">
-          <a href="https://open.spotify.com/show/5Mig7pzSX0z5fairbQaeZu?si=ES-SWDUrQTSKUDjPxM_bWw"><img src="/images/icon_spot.svg" alt="Spotify" className="w-[36px] h-[36px]" /></a>
-        </div>
-        <div className="bg-socialDiv w-[36px] h-[36px] rounded">
-          <a href="https://podcasts.apple.com/cz/podcast/za-bohat%C5%A1%C3%AD-%C4%8Desko/id1542800787"><img src="/images/icon_app.svg" alt="Apple Podcasts" className="w-[36px] h-[36px]" /></a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <hr className="border-darkDivGrey w-full max-w-[1392px] mx-auto"/>
-  <div className="flex flex-col md:flex-row w-full items-center max-w-[1392px] mx-auto py-8 md:py-12 ">
-    <p className="text-silkBeige text-center mx-auto">© 2025 Za bohatší Česko | Všechna práva vyhrazena | <a href="https://www.zabohatsicesko.cz/ochrana-osobnich-udaju/">Ochrana osobních údajů</a> | <a href="https://www.zabohatsicesko.cz/ochrana-osobnich-udaju/">Cookies</a></p>
-  </div>
-</footer>
-
-
-
-
-
-
-
-
     </main>
   );
 }
