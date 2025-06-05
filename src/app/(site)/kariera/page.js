3a2575b56103fe5a4c7fee1f3fb6f2a17edd6e3e
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-05 23:18:09 +0200

    recenze

--- src/app/(site)/kariera/page.js
+++ src/app/(site)/kariera/page.js
@@ -7,14 +7,45 @@ export default async function karieraPage() {
   return (
     <main className="relative z-100">
 
+        <section className="px-4 w-full py-12 md:py-24 bg-silverSage recenze">
+  <div className="w-full max-w-[1392px] mx-auto text-center">
+
+    <h2 className="text-[28px] md:text-[40px] pb-8 md:pb-10 text-white text-center">Chceš i ty mít reputaci jako my?</h2>
+
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+  {recenze.map((item) => (
+    <div
+      key={item.id}
+      className="relative bg-gradient-to-b pt-10 pl-10 ml-2 md:ml-6 mb-2 md:mb-0 rounded-lg flex flex-row gap-5 items-start overflow-visible"
+    >
+      {/* Nový absolutní obrázek */}
+      <img
+        src="/images/symbol-golden-s.svg" // ← nahraď svým obrázkem
+        alt="Dekorace"
+        className="absolute top-0 left-0 w-[46px] h-[46px] -translate-x-1/2 -translate-y-1/2 z-10"
+      />
+
+      <p className="text-left text-raisinBlack w-1/2">
+        {item.acf?.citation || 'Bez citace'}
+      </p>
+
+      <img
+        src={item._embedded?.['wp:featuredmedia']?.[0]?.source_url || '/placeholder.jpg'}
+        alt="Recenze"
+        className="w-[200px] mt-[-80px] h-auto object-contain align-middle"
+      />
+    </div>
+  ))}
+</div>
+    <a href="https://zabohatsicesko.cz/reference" className="custom-btn py-3 px-4 rounded bg-goldenBrown text-silkBeige mt-8 inline-block text-center">Přečíst více recenzí</a>
+  </div>
+</section>
+
       <section className="bg-silkBeige w-full py-12 md:py-16">
   <h2 className="text-[28px] md:text-[40px] text-goldenBrown text-center">Přidej se k nám!</h2>
   <p className="text-center text-raisinBlack">Chcete mít ve financích jasno a klid? <strong>Začněte tady.</strong></p>
   
   <div className="flex flex-col w-full max-w-[1392px] mx-auto py-4 md:py-8 justify-center">
-
-
-    
 
     <form
   action="https://formcarry.com/s/kY_1MuRL2um"
