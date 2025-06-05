3669ce83e422ace94fd2c9ac0140e46222d482b4
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-05 23:45:10 +0200

    tym

--- src/app/(site)/nas-tym/page.js
+++ src/app/(site)/nas-tym/page.js
@@ -1,12 +1,19 @@
-export default async function tymPage() {
+export default async function TymPage() {
   const tymRes = await fetch(
     'https://api.zabohatsicesko.cz/wp-json/wp/v2/tym?per_page=100&_embed'
   );
-  const recenze = await tymRes.json();
+  const tymPosts = await tymRes.json();
 
   return (
     <main>
+      {tymPosts.length === 0 && <p>Žádná data k zobrazení.</p>}
 
+      {tymPosts.map((post) => (
+        <article key={post.id}>
+          <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+          <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+        </article>
+      ))}
     </main>
   );
 }
