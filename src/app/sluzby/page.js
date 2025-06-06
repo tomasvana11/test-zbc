9afc8c4d59ddd524bd405a162aa8cd528707fb58
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-06 21:03:27 +0200

    ff

--- src/app/sluzby/page.js
+++ src/app/sluzby/page.js
@@ -1,8 +1,7 @@
 export default function SluzbyPage() {
   return (
-    <section className="p-6 max-w-4xl mx-auto">
-      <h2 className="text-2xl font-semibold mb-4">Obsah stránky Služby</h2>
-      <p>Zde můžeš přidat obsah, který se zobrazí pod headerem.</p>
-    </section>
+    <main className="flex min-h-screen flex-col items-center bg-white">
+        
+    </main>
   );
 }
