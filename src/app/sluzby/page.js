05be1aab516cd2d479768233186cca627fccecd7
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-07 00:07:33 +0200

    pozice

--- src/app/sluzby/page.js
+++ src/app/sluzby/page.js
@@ -264,10 +264,10 @@ export default async function SluzbyPage() {
       <div className="w-full lg:w-2/3 grid grid-cols-1 gap-8 md:gap-10">
         {/* Karta 1 */}
         <div className="relative flex flex-col md:flex-row gap-6">
+            <div className="shrink-0 rounded-br-[12px] w-[64px] h-[64px] md:w-[80px] md:h-[80px] bg-silkBeige flex items-center justify-center">
             <img src="/images/symbol-golden-single.svg"
             alt="Symbol"
             className="absolute w-auto h-[32px] md:h-[48px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"/>
-            <div className="shrink-0 rounded-br-[12px] w-[64px] h-[64px] md:w-[80px] md:h-[80px] bg-silkBeige flex items-center justify-center">
             <span className="recife text-[28px] md:text-[36px] text-raisinBlack text-center">01</span>
             </div>
             <div>
