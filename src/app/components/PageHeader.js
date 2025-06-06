7974cdc99349c7e1f7cf70bf0764954a3a01f673
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-07 00:40:07 +0200

    pxHeader fix

--- src/app/components/PageHeader.js
+++ src/app/components/PageHeader.js
@@ -1,7 +1,7 @@
 export default function PageHeader({ title, description }) {
   return (
     <section
-      className="relative w-full overflow-hidden bg-[url('/images/heroimage.png')] bg-no-repeat bg-cover md:bg-[length:120%_auto] bg-[right_center] pt-[76px] md:pt-[100px]"
+      className="relative w-full overflow-hidden bg-[url('/images/heroimage.png')] bg-no-repeat bg-cover md:bg-[length:120%_auto] bg-[right_center] pt-[76px] md:pt-[100px] px-4"
     >
       {/* Gradient */}
       <div
