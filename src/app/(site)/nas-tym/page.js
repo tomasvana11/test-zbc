8194b3664abc8bd810ad751ad9234b9ae9b7e629
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-06 08:34:08 +0200

    test

--- src/app/(site)/nas-tym/page.js
+++ src/app/(site)/nas-tym/page.js
@@ -27,7 +27,7 @@ export default async function TymPage() {
   return (
     <main className="flex">
       <section className="px-4 w-full">
-        <div className="max-w-[1392px] mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-10">
+        <div className="max-w-[1392px] mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-x-10 gap-y-14">
           {members.map((member) => (
             <article
               key={member.id}
