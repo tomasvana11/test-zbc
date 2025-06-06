eaf6fd5dc50dcc274b3c90446af2d0e082cce1a3
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-06 21:30:00 +0200

    dfvG

--- src/app/sluzby/page.js
+++ src/app/sluzby/page.js
@@ -14,6 +14,18 @@ export default async function SluzbyPage() {
   // Z ACF polí vezmi, co potřebuješ
   const serv_intro_title = page.acf?.serv_intro_title || '';
   const serv_intro_desc = page.acf?.serv_intro_desc || '';
+  const serv_adv_title= page.acf?.serv_adv_title || '';
+  const serv_adv_1= page.acf?.serv_adv_1 || '';
+  const serv_adv_1_desc= page.acf?.serv_adv_1_desc || '';
+  const serv_adv_2= page.acf?.serv_adv_2 || '';
+  const serv_adv_2_desc= page.acf?.serv_adv_2_desc || '';
+  const serv_adv_3= page.acf?.serv_adv_3 || '';
+  const serv_adv_3_desc= page.acf?.serv_adv_3_desc || '';
+  const serv_adv_4= page.acf?.serv_adv_4 || '';
+  const serv_adv_4_desc= page.acf?.serv_adv_4_desc || '';
+  const serv_adv_5= page.acf?.serv_adv_5 || '';
+  const serv_adv_5_desc= page.acf?.serv_adv_5_desc || '';
+
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-white">
@@ -52,6 +64,52 @@ export default async function SluzbyPage() {
           </div>
         </div>
       </section>
+
+      <section className="bg-raisinBlack w-full  px-4 py-12 md:py-16">
+
+        <div className="flex flex-col md:flex-row w-full items-center max-w-[1392px] mx-auto">
+
+          <div className="w-full">
+
+            <h2 className="text-[28px] md:text-[40px] pb-8 md:pb-10 text-white text-center">{serv_adv_title}</h2>
+
+            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
+              <div className="bg-cardGrey p-5 rounded-lg flex flex-col gap-3">
+                <img src="/images/zbc-symbol-golden.svg" alt="Symbol Gold" className="w-[32px] h-[32px]" />
+                <h4 className="card-heading text-goldenBrown mt-2">{serv_adv_1}</h4>
+                <p className="text-silkBeige">{serv_adv_1_desc}</p>
+              </div>
+              <div className="bg-cardGrey p-5 rounded-lg flex flex-col gap-3">
+                <img src="/images/zbc-symbol-silver.svg" alt="Symbol Silver" className="w-[32px] h-[32px]" />
+                <h4 className="card-heading text-silverSage mt-2">{serv_adv_2}</h4>
+                <p className="text-silkBeige">{serv_adv_2_desc}</p>
+              </div>
+              <div className="bg-cardGrey p-5 rounded-lg flex flex-col gap-3">
+                <img src="/images/zbc-symbol-golden.svg" alt="Symbol Gold" className="w-[32px] h-[32px]" />
+                <h4 className="card-heading text-goldenBrown mt-2">{serv_adv_3}</h4>
+                <p className="text-silkBeige">{serv_adv_3_desc}</p>
+              </div>
+              <div className="bg-cardGrey p-5 rounded-lg flex flex-col gap-3">
+                <img src="/images/zbc-symbol-silver.svg" alt="Symbol Silver" className="w-[32px] h-[32px]" />
+                <h4 className="card-heading text-silverSage mt-2">{serv_adv_4}</h4>
+                <p className="text-silkBeige">{serv_adv_4_desc}</p>
+              </div>
+              <div className="bg-cardGrey p-5 rounded-lg flex flex-col gap-3">
+                <img src="/images/zbc-symbol-golden.svg" alt="Symbol Gold" className="w-[32px] h-[32px]" />
+                <h4 className="card-heading text-goldenBrown mt-2">{serv_adv_5}</h4>
+                <p className="text-silkBeige">{serv_adv_5_desc}</p>
+              </div>
+            </div>
+
+          </div>
+
+          
+
+        </div>
+
+      </section>
+
+
     </main>
   );
 }
