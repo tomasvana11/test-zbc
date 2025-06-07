9b59b0965ce156959321b730553ebe63420f08cb
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-07 09:51:55 +0200

    fff

--- src/app/(site)/kariera/layout.js
+++ src/app/(site)/kariera/layout.js
@@ -6,15 +6,17 @@ import PageHeader from '../../components/PageHeader';
 export default function KarieraLayout({ children }) {
   const pathname = usePathname();
 
-  // Pokud je detail pozice (např. /kariera/asistentka), nevykresluj header zde
-  const isSlugPage = pathname !== '/kariera' && pathname.startsWith('/kariera');
+  const isDetailPage = pathname.startsWith('/kariera/') && pathname !== '/kariera';
 
   return (
     <>
-      {!isSlugPage && (
-        <PageHeader title="Máš na to stát se naším parťákem?" description="Hledáme osobnosti, které chtějí růst. Lidi, pro které práce není jen výdělek, ale příležitost měnit životy – vlastní i těch, se kterými spolupracují. U nás najdete silné zázemí, přátelské prostředí a prostor tvořit vlastní cestu, projekty i výsledky na které budete hrdí." />
+      {!isDetailPage && (
+        <PageHeader
+          title="Máš na to stát se naším parťákem?"
+          description="Hledáme osobnosti, které chtějí růst. Lidi, pro které práce není jen výdělek, ale příležitost měnit životy – vlastní i těch, se kterými spolupracují. U nás najdete silné zázemí, přátelské prostředí a prostor tvořit vlastní cestu, projekty i výsledky na které budete hrdí."
+        />
       )}
-      <main>{children}</main>
+      <main className="relative z-0">{children}</main>
     </>
   );
 }
