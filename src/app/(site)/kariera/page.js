85504a0e4a83cdeef766a0c696b039038d24416a
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-07 09:45:23 +0200

    sdd

--- src/app/(site)/kariera/page.js
+++ src/app/(site)/kariera/page.js
@@ -6,7 +6,7 @@ export default async function KarieraPage() {
   const recenzeRes = await fetch('https://api.zabohatsicesko.cz/wp-json/wp/v2/recenze?per_page=3&_embed');
   const poziceRes = await fetch('https://api.zabohatsicesko.cz/wp-json/wp/v2/pracovni-pozice');
 
-  const { career_intro_title, career_intro_detail, career_intro_img } = kariera.acf;
+  const { career_intro_title = '', career_intro_detail = '', career_intro_img = '' } = kariera.acf || {};
 
   const recenze = await recenzeRes.json();
   const pozice = await poziceRes.json();
@@ -17,7 +17,7 @@ export default async function KarieraPage() {
       <section className="px-4 w-full -mt-8 md:-mt-20 z-[50]">
         <div className="flex flex-col lg:flex-row items-end w-full max-w-[1392px] mx-auto bg-white xl:bg-transparent">
           <div className="flex w-full lg:w-1/2 mr-0 xl:mr-6 justify-center items-center bg-white xl:bg-transparent">
-            <img src={career_intro_img} alt="obrázek" className="w-full h-auto object-contain" />
+            <img src={career_intro_img} alt={career_intro_img.alt} className="w-full h-auto object-contain" />
           </div>
           <div className="w-full lg:w-1/2 mt-8 lg:mt-0 lg:pl-12 lg:mb-12">
             <h2 className="text-[28px] lg:text-[40px] mb-4 text-goldenBrown">{career_intro_title}</h2>
