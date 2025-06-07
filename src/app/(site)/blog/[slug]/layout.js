60c4aa2feaf1c4671c1e299efdeba3f2040248b7
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-07 22:39:11 +0200

    typo

--- src/app/(site)/blog/[slug]/layout.js
+++ src/app/(site)/blog/[slug]/layout.js
@@ -1,4 +1,4 @@
-import PageHeader from '../../../components/PageHeader';
+import PageHeader from '../../../components/BlogHeader';
 
 export default async function PostLayout({ children, params }) {
   const { slug } = params;
