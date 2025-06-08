0aecf95110e885c77ef22756ae2f0b8ffe73e037
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-08 10:09:03 +0200

    idk

--- src/app/(site)/blog/page.js
+++ src/app/(site)/blog/page.js
@@ -55,14 +55,16 @@ export default async function BlogPage() {
                     className="w-full h-52 object-cover rounded-lg"
                     />
                     <div className="px-6 pt-8 flex flex-col flex-grow justify-between">
-                        <h3
-                        className="text-lg text-goldenBrown text-[22px] font-satoshi-bold mb-5 group-hover:underline"
-                        dangerouslySetInnerHTML={{ __html: post.title }}
-                        />
-                        <div
-                        className="text-raisinBlack"
-                        dangerouslySetInnerHTML={{ __html: post.excerpt }}
-                        />
+                        <div>
+                            <h3
+                            className="text-lg text-goldenBrown text-[22px] font-satoshi-bold mb-5 group-hover:underline"
+                            dangerouslySetInnerHTML={{ __html: post.title }}
+                            />
+                            <div
+                            className="text-raisinBlack"
+                            dangerouslySetInnerHTML={{ __html: post.excerpt }}
+                            />
+                        </div>
                         <div className="flex flex-col gap-5 pt-5 pb-6">
                             <hr className="border-t border-raisinBlack/10" />
                             <div className="mt-auto flex justify-between items-center text-goldenBrown">
