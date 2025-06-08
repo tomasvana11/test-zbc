fd4adbd68c91f12470b2ba9665e4f6cc902ab956
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-08 15:14:58 +0200

    ted uz

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css'
 import Footer from './components/Footer'
-import Menu from './components/Menu'  // importuj Menu
 
 export default function RootLayout({ children }) {
   return (
