8034edc73284628bda1fe2877ed8a2874e1ff750
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-08 17:41:02 +0200

    /

--- src/app/(site)/kariera/page.js
+++ src/app/(site)/kariera/page.js
@@ -59,7 +59,7 @@ export default async function CareerPage() {
                 className="text-raisinBlack"
                 dangerouslySetInnerHTML={{ __html: career_intro_detail }}
               />
-              <a href="https://zabohatsicesko.cz/nas-tym"
+              <a href="/nas-tym"
               className="custom-btn py-3 px-4 rounded bg-goldenBrown text-silkBeige mt-8 inline-block text-center">
                 Chci poznat celý tým
               </a>
