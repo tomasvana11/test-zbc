bb9ba96d2f4e14d3ebfa5962ec8b631649ae221d
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-09 08:01:44 +0200

    update

--- src/app/(site)/kariera/page.js
+++ src/app/(site)/kariera/page.js
@@ -284,7 +284,7 @@ export default async function CareerPage() {
     </div>
   ))}
 </div>
-    <a href="https://zabohatsicesko.cz/reference" className="custom-btn py-3 px-4 rounded bg-goldenBrown text-silkBeige mt-8 inline-block text-center">Přečíst více recenzí</a>
+    <a href="https://test-zbc-ten.vercel.app/reference" className="custom-btn py-3 px-4 rounded bg-goldenBrown text-silkBeige mt-8 inline-block text-center">Přečíst více recenzí</a>
   </div>
         </section>
 
