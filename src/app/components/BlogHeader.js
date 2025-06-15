da1f9a93dece698553871d19c50c2b9ecb2220fe
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-15 11:44:01 +0200

    webp

--- src/app/components/BlogHeader.js
+++ src/app/components/BlogHeader.js
@@ -1,7 +1,7 @@
 export default function PageHeader({ title, date, imageUrl }) {
   return (
     <section
-      className="relative w-full min-h-[500px] overflow-hidden bg-[url('/images/heroimage.png')] bg-no-repeat bg-cover bg-center pt-[76px] md:pt-[100px] px-4"
+      className="relative w-full min-h-[500px] overflow-hidden bg-[url('/images/heroimage.webp')] bg-no-repeat bg-cover bg-center pt-[76px] md:pt-[100px] px-4"
     >
       {/* Gradient */}
       <div
