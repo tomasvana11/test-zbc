a2c02ca7830b14fd3adeb247a6f95f4c8b7b85c9
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-06-15 11:52:27 +0200

    head

---
+++ src/app/(site)/kontakt/head.js
@@ -0,0 +1,18 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="preload"
+        href="/images/heroimage.webp"
+        as="image"
+        type="image/webp"
+      />
+      <link
+        rel="preload"
+        href="/images/symbol-outline.svg"
+        as="image"
+        type="image/svg+xml"
+      />
+    </>
+  );
+}
